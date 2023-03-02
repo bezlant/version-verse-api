ae73fdea821bf2030437c90ee1b31dd36ab1b78a
bezlant <abezludniy@mail.ru>
2023-03-02 08:06:17 +0300

    feat: Added cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 import app from './server'
 
-app.listen(3001, () => {
-	console.log('hello on http://localhost:3001')
-})
+app.listen(3001, () => { })
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import express from 'express'
 import router from './router'
 import morgan from 'morgan'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+// GET http://localhost:3001/api/product
 
 app.use('/api', router)
 
