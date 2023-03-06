17e8e541f4e40cfa04a4a12027f04c0b967a9d72
bezlant <abezludniy@mail.ru>
2023-03-06 08:12:10 +0300

    feat: Added a handler for creating a new user

---
+++ src/handlers/user.ts
@@ -0,0 +1,15 @@
+import prisma from '@/db'
+import { Request, Response } from 'express'
+import { createJWT, hashPassword } from '../modules/auth'
+
+export const createNewUser = async (req: Request, res: Response) => {
+  const user = await prisma.user.create({
+    data: {
+      username: req.body.username,
+      password: await hashPassword(req.body.password),
+    },
+  })
+
+  const token = createJWT(user)
+  res.json({ token })
+}
--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,46 +1,49 @@
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express'
+import bcrypt from 'bcrypt'
 import jwt, { JwtPayload } from 'jsonwebtoken'
 
-declare module "express-serve-static-core" {
-	interface Request {
-		user: string | JwtPayload;
-	}
-}
+const JWT_SECRET = process.env.JWT_SECRET
+if (!JWT_SECRET) throw new Error('JWT_SECRET must be defined')
 
-const JWT_SECRET = process.env.JWT_SECRET;
-if (!JWT_SECRET)
-	throw new Error('JWT_SECRET must be defined')
+const PASSWORD_SALT = process.env.PASSWORD_SALT
+if (!PASSWORD_SALT) throw new Error('PASSWORD_SALT must be defined')
 
-export const createJWT = (user: { id: string, username: string }) => {
-	const token = jwt.sign({ id: user.id, username: user.username }, JWT_SECRET)
-	return token
+export const comparePasswords = (password: string, hash: string) =>
+  bcrypt.compare(password, hash)
+
+export const hashPassword = (password: string) =>
+  bcrypt.hash(password, PASSWORD_SALT)
+
+export const createJWT = (user: { id: string; username: string }) => {
+  const token = jwt.sign({ id: user.id, username: user.username }, JWT_SECRET)
+  return token
 }
 
 export const protect = (req: Request, res: Response, next: NextFunction) => {
-	const bearer = req.headers.authorization
+  const bearer = req.headers.authorization
 
-	if (!bearer) {
-		res.status(401)
-		res.send({ message: 'Not authorized' })
-		return;
-	}
+  if (!bearer) {
+    res.status(401)
+    res.send({ message: 'Not authorized' })
+    return
+  }
 
-	const [, token] = bearer.split(" ")
+  const [, token] = bearer.split(' ')
 
-	if (!token) {
-		res.status(401)
-		res.send({ message: 'Not valid token' })
-		return;
-	}
+  if (!token) {
+    res.status(401)
+    res.send({ message: 'Not valid token' })
+    return
+  }
 
-	try {
-		const user = jwt.verify(token, JWT_SECRET)
-		req.user = user
-	} catch (e) {
-		console.error(e)
-		res.status(401)
-		res.send({ message: 'Not valid token' })
-	}
+  try {
+    const user = jwt.verify(token, JWT_SECRET)
+    req.user = user
+  } catch (e) {
+    console.error(e)
+    res.status(401)
+    res.send({ message: 'Not valid token' })
+  }
 
-	next();
+  next()
 }
