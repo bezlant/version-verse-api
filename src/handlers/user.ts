1f9e46c1e89ba963ef56f2842648e5a9c91f9f4c
bezlant <abezludniy@mail.ru>
2023-03-07 07:48:53 +0300

    feat: Added signin handler

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,6 +1,7 @@
 import prisma from '@/db'
 import { Request, Response } from 'express'
-import { createJWT, hashPassword } from '@/modules/auth'
+import { comparePasswords, createJWT, hashPassword } from '@/modules/auth'
+import { compare } from 'bcrypt'
 
 export const createNewUser = async (req: Request, res: Response) => {
   const user = await prisma.user.create({
@@ -11,4 +12,29 @@ export const createNewUser = async (req: Request, res: Response) => {
   })
   const token = createJWT(user)
   res.json({ token })
+}
+
+export const signIn = async (req: Request, res: Response) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      username: req.body.username,
+    },
+  })
+
+  if (!user) {
+    res.status(404)
+    res.json({ message: 'User not found' })
+    return
+  }
+
+  const isValid = await comparePasswords(req.body.password, user?.password)
+
+  if (!isValid) {
+    res.status(401)
+    res.json({ message: 'Invalid Password' })
+    return
+  }
+
+  const token = createJWT(user)
+  return token
 }
