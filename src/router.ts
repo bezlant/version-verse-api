02eba4eda53a9da3e2302bc00d96e5fb28893fbe
bezlant <abezludniy@mail.ru>
2023-03-13 07:48:27 +0300

    feat: Added some api routes validation

--- src/router.ts
+++ src/router.ts
@@ -1,25 +1,73 @@
 import {Router} from 'express'
+import {body} from 'express-validator'
+import {handleInputErrors} from './modules/middleware'
 
 const router = Router()
 
-router.get('/product', (req, res) => {
-  res.json({message: 'hello'})
-})
-router.post('/product', (req, res) => {})
+// Product
+router.get('/product', (req, res) => {})
+
 router.get('/product/:id', (req, res) => {})
-router.put('/product/:id', (req, res) => {})
+
+router.post(
+  '/product',
+  body('name').isString(),
+  handleInputErrors,
+  (req, res) => {}
+)
+
+router.put(
+  '/product/:id',
+  body('name').isString(),
+  handleInputErrors,
+  (req, res) => {}
+)
+
 router.delete('/product/:id', (req, res) => {})
 
+// Update
 router.get('/update', (req, res) => {})
-router.post('/update', (req, res) => {})
+
 router.get('/update/:id', (req, res) => {})
-router.put('/update/:id', (req, res) => {})
+
+router.put(
+  '/update/:id',
+  body('title').optional(),
+  body('body').optional(),
+  body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']),
+  body('version').optional(),
+  (req, res) => {}
+)
+
+router.post(
+  '/update',
+  body('title').exists().isString(),
+  body('body').exists().isString(),
+  (req, res) => {}
+)
+
 router.delete('/update/:id', (req, res) => {})
 
+// Update point
 router.get('/updatepoint', (req, res) => {})
-router.post('/updatepoint', (req, res) => {})
+
 router.get('/updatepoint/:id', (req, res) => {})
-router.put('/updatepoint/:id', (req, res) => {})
+
+router.put(
+  '/updatepoint/:id',
+  body('name').optional().isString(),
+  body('description').optional().isString(),
+  (req, res) => {}
+)
+
+router.post(
+  '/updatepoint',
+  body('name').exists().isString(),
+  body('description').isString(),
+  body('updateId').exists().isString(),
+  (req, res) => {}
+)
+
 router.delete('/updatepoint/:id', (req, res) => {})
 
 export default router
